7f7ef22c84299aada35ee842a20a88f8d1cb7c40
kajalvanzara05-png <kajalvanzara05@gmail.com>
2025-12-01 13:57:29 +0530

    backendcontact

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,21 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function store(Request $request)
+{
+    $validated = $request->validate([
+        'name' => 'required',
+        'email' => 'required|email',
+        'phone' => 'required',
+        'message' => 'required',
+    ]);
+
+    $contact = Contact::create($validated);
+
+    return response()->json([
+        'status' => true,
+        'message' => 'Contact submitted successfully'
+    ]);
+}
+
 }
