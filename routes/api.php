bd395e8663da69d964d9a6968c679b77ab920b97
hiral2610 <hiralthakor2610@gmail.com>
2025-12-03 09:35:38 +0530

    blog

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
-Route::get('/user', function (Request $request) {
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
+
+
+Route::get('/blogs', [BlogController::class, 'index']);
+
+
+Route::get('/blogs/{slug}', [BlogController::class, 'show']);
+
+Route::middleware('auth:sanctum')->post('/blogs', [BlogController::class, 'store']);
